d715d0f61bf4bf78c2ef49b09d9c4ee013a4b32f
LameckVieira <lameck.v.barbosa@gmail.com>
2020-11-18 10:38:51 -0300

    tabuada

--- Program.cs
+++ Program.cs
@@ -10,65 +10,77 @@ namespace Calculadora
             // Pedir o 1° numero
             // pedir o 2° numero
 
-            Console.WriteLine("Digite a operação que deseja fazer \n Soma, Subtracao, Multiplicacao, Divisao");
-            string operacao = Console.ReadLine();
+            // Console.WriteLine("Digite a operação que deseja fazer \n Soma, Subtracao, Multiplicacao, Divisao");
+            // string operacao = Console.ReadLine();
 
-            Console.WriteLine("Digite o 1º numero");
-            float num1 = float.Parse(Console.ReadLine());
+            // Console.WriteLine("Digite o 1º numero");
+            // float num1 = float.Parse(Console.ReadLine());
 
-            // ALT + SHIFT + Seta para baixo
-            Console.WriteLine("Digite o 2º numero");
-            float num2 = float.Parse(Console.ReadLine());
+            // // ALT + SHIFT + Seta para baixo
+            // Console.WriteLine("Digite o 2º numero");
+            // float num2 = float.Parse(Console.ReadLine());
 
-            float resultado = 0;
+            // float resultado = 0;
             
-            // if(operacao == "Soma"){
+            // // if(operacao == "Soma"){
 
-            // }else if(operacao == "Subtracao"){
-            //     resultado = num1 / num2;
-            // }else if(operacao == "Multiplicacao"){
-            //     resultado = num1 - num2;
-            // }else if(operacao == "Divisao"){
-            //     resultado = num1 / num2;
-            // }else{
-            //     Console.WriteLine("Operação invalida!");
-            // }
+            // // }else if(operacao == "Subtracao"){
+            // //     resultado = num1 / num2;
+            // // }else if(operacao == "Multiplicacao"){
+            // //     resultado = num1 - num2;
+            // // }else if(operacao == "Divisao"){
+            // //     resultado = num1 / num2;
+            // // }else{
+            // //     Console.WriteLine("Operação invalida!");
+            // // }
 
-            switch(operacao){
+            // switch(operacao){
 
-                case "Soma":
-                    resultado = num1+num2;
-                    break;
+            //     case "Soma":
+            //         resultado = num1+num2;
+            //         break;
 
-                case "Subtracao":
-                    resultado = num1-num2;
-                    break;
+            //     case "Subtracao":
+            //         resultado = num1-num2;
+            //         break;
 
-                case "Multiplicacao":
-                    resultado = num1*num2;
-                    break;
+            //     case "Multiplicacao":
+            //         resultado = num1*num2;
+            //         break;
 
-                case "Divisao":
-                    resultado = num1/num2;
-                break;
+            //     case "Divisao":
+            //         resultado = num1/num2;
+            //     break;
 
-                default:
-                    Console.WriteLine("Operacao invalida");
+            //     default:
+            //         Console.WriteLine("Operacao invalida");
 
 
                     
-                 break;
+            //      break;
 
 
 
-                // Concatenação
-                //Console.WriteLine("Calculo : "+num1" e "+num2+" = Resultado : "+resultado");
+            //     // Concatenação
+            //     //Console.WriteLine("Calculo : "+num1" e "+num2+" = Resultado : "+resultado");
 
-                // Interpolação
+            //     // Interpolação
           
                 
+            // }
+            // Console.WriteLine($"Calculo: {num1} e {num2} = Resultado : {resultado}"); 
+
+            Console.WriteLine("Tabuada");
+            Console.WriteLine(" ");
+            Console.WriteLine("Qual tabuada?");
+
+            int tabuada = int.Parse(Console.ReadLine());
+            int contador = 0;
+            while(contador <= 10){
+                Console.WriteLine(tabuada * contador);
+                contador = contador +1;
             }
-            Console.WriteLine($"Calculo: {num1} e {num2} = Resultado : {resultado}");          
+                     
         }
     }
 }
